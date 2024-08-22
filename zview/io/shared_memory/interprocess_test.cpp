18acfe94e0a01b5970f1c2ec6ed6c58c12b9ddc0
Yannick Brunken <y.brunken@gmail.com>
2024-08-22 18:47:39 +0200

    Run pre-commit

--- zview/io/shared_memory/interprocess_test.cpp
+++ zview/io/shared_memory/interprocess_test.cpp
@@ -5,10 +5,10 @@
 #include <boost/interprocess/sync/named_mutex.hpp>
 
 int main() {
-    boost::interprocess::named_mutex mtx{boost::interprocess::open_or_create,
+  boost::interprocess::named_mutex mtx{boost::interprocess::open_or_create,
                                        "rules_boost_interprocess_test"};
   mtx.lock();
   mtx.unlock();
-    
-    return 0;
+
+  return 0;
 }
