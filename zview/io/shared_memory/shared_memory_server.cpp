a8736d1f5cf2b8cbc85df144d18d3162a4fbdfba
Yannick Brunken <y.brunken@gmail.com>
2024-08-25 21:51:37 +0200

    Add shared memory server

--- zview/io/shared_memory/shared_memory_server.cpp
+++ zview/io/shared_memory/shared_memory_server.cpp
@@ -1,5 +1,6 @@
 #include "zview/io/shared_memory/shared_memory_server.h"
 
+#include <boost/interprocess/sync/scoped_lock.hpp>
 #include <cstring>
 #include <iostream>  // Add this line
 #include <iterator>
@@ -8,6 +9,7 @@
 #include "zview/utils/recast.h"
 
 namespace zview {
+
 template <typename T>
 void getAndAdvance(std::uint8_t *&ptr, std::vector<T> &vals) {
   const std::size_t sz = *recast<std::size_t *>(ptr);
@@ -17,7 +19,7 @@ void getAndAdvance(std::uint8_t *&ptr, std::vector<T> &vals) {
   std::advance(ptr, sz * sizeof(T));
 }
 
-bool SharedMemoryServer::call_callback(std::uint8_t *ptr) {
+bool SharedMemoryServer::call_callback(std::uint8_t *&ptr) {
   const auto type = *recast<zview::SharedMemMessageType *>(ptr);
   std::advance(ptr, sizeof(zview::SharedMemMessageType));
   const std::string name(recast<const char *>(ptr), MAX_NAME_LENGTH);
@@ -56,13 +58,58 @@ bool SharedMemoryServer::call_callback(std::uint8_t *ptr) {
       return false;
   }
 
-  std::cout << "Processed message" << std::endl;
   return true;
 }
 
 SharedMemoryServer::SharedMemoryServer(const AddShape &addShape,
                                        const RemoveShape &removeShape)
-    : m_addShape{addShape}, m_removeShape{removeShape} {}
+    : m_addShape{addShape},
+      m_removeShape{removeShape},
+      m_shm{boost::interprocess::create_only, SHARED_MEMORY_NAME,
+            boost::interprocess::read_write} {
+  if (SHARED_MEMORY_SIZE <= sizeof(SharedMemoryInfo)) {
+    throw std::runtime_error("Shared memory size too small");
+  }
 
-void SharedMemoryServer::step() {}
+  m_shm.truncate(SHARED_MEMORY_SIZE);
+  m_region = boost::interprocess::mapped_region(
+      m_shm, boost::interprocess::read_write);
+  std::memset(m_region.get_address(), 0, m_region.get_size());
+
+  SharedMemoryInfo *info =
+      static_cast<SharedMemoryInfo *>(m_region.get_address());
+
+  info->read_offset = sizeof(SharedMemoryInfo);
+  info->write_offset = info->read_offset;
+}
+
+SharedMemoryServer::~SharedMemoryServer() {
+  boost::interprocess::shared_memory_object::remove(SHARED_MEMORY_NAME);
+}
+
+void SharedMemoryServer::step() {
+  SharedMemoryInfo *info =
+      static_cast<SharedMemoryInfo *>(m_region.get_address());
+
+  boost::interprocess::scoped_lock<boost::interprocess::interprocess_mutex>
+      lock(info->mutex);
+  auto ptr =
+      static_cast<std::uint8_t *>(m_region.get_address()) + info->read_offset;
+
+  if (*ptr == 0) {
+    return;
+  }
+
+  if (call_callback(ptr)) {
+    info->read_offset =
+        std::distance(static_cast<std::uint8_t *>(m_region.get_address()), ptr);
+  } else {
+    std::uint8_t *end = static_cast<std::uint8_t *>(m_region.get_address()) +
+                        m_region.get_size();
+    std::memset(ptr, 0, std::distance(ptr, end));
+
+    info->write_offset = info->read_offset;
+  }
+}
+
 }  // namespace zview
--- zview/io/shared_memory/shared_memory_server.h
+++ zview/io/shared_memory/shared_memory_server.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <boost/interprocess/mapped_region.hpp>
+#include <boost/interprocess/shared_memory_object.hpp>
 #include <functional>
 
 #include "zview/types/types.h"
+
 namespace zview {
+
 class SharedMemoryServer {
   using AddShape = std::function<std::uint32_t(types::Shape &&)>;
   using RemoveShape = std::function<void(const std::string &)>;
@@ -11,11 +15,20 @@ class SharedMemoryServer {
   AddShape m_addShape;
   RemoveShape m_removeShape;
 
-  bool call_callback(std::uint8_t *ptr);
+  boost::interprocess::shared_memory_object m_shm;
+  boost::interprocess::mapped_region m_region;
+
+  bool call_callback(std::uint8_t *&ptr);
 
  public:
   explicit SharedMemoryServer(const AddShape &addShape,
                               const RemoveShape &removeShape);
+  SharedMemoryServer(const SharedMemoryServer &) = delete;
+  SharedMemoryServer &operator=(const SharedMemoryServer &) = delete;
+  SharedMemoryServer(SharedMemoryServer &&) = delete;
+  SharedMemoryServer &operator=(SharedMemoryServer &&) = delete;
+  ~SharedMemoryServer();
+
   void step();
 };
 
