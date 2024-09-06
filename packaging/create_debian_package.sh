bf2418d721079474b6dd87494789251acb154294
Ohad Menashe <ohad.men@gmail.com>
2024-09-07 00:45:24 +0200

    add pypi deploy

--- packaging/create_debian_package.sh
+++ packaging/create_debian_package.sh
@@ -1,13 +1,6 @@
 #! /bin/bash
-#Versdion should be change in:
-# *here@8
-# *here@37
-# create_msi_packge.iss @10
-# *README.md
-# *zview/gui/main_window.cpp@129
-# *zview/gui/zview_main.cpp
-
-ver=1.50 
+bz build //zview:zview_binary
+ver=2.0.0 
 rm -rf pack
 mkdir pack 
 cd pack
@@ -15,17 +8,9 @@ taget_base=zview-$ver
 
 target_dir=$taget_base/opt/zview
 mkdir -p $target_dir
-cp ../../build/zview $target_dir
+cp ../../bazel-bin/zview/zview_binary $target_dir/zview
 cp ../zview.png $target_dir
 
-
-target_dir=$taget_base/usr/lib/x86_64-linux-gnu
-mkdir -p $target_dir
-cp -P ../../build/libzview_inf.so* $target_dir
-
-target_dir=$taget_base/usr/include/
-mkdir -p $target_dir
-cp ../../zview/interface/zview_inf.h $target_dir
 
 target_dir=$taget_base/usr/share/applications/
 mkdir -p $target_dir
@@ -45,17 +30,11 @@ echo "Standards-Version: 4.1.2" >> $control_file
 echo "Homepage: https://github.com/ohadmen/zview" >> $control_file
 echo "Package: zview" >> $control_file
 echo "Architecture: amd64" >> $control_file
-echo "Depends: qt5-default" >> $control_file
 echo "Description: Generic mesh&cloud visualization tool" >> $control_file
-echo " Zview - a general 3d view (version 1.41)" >> $control_file
-echo " Zview was created as a tool to reflect the true state of 3d point cloud/mesh data stored in a file or on the heap." >> $control_file
-echo " Implementation was written by modern c++ and OpenGL ES, with an effort to minimize cpu load and memory signiture." >> $control_file
-echo " App can open a layered ply file - a ply file that contains multiple ply files, with hirarchical layer names." >> $control_file
-echo " In addition, app support c++, Python, Matlab(WIP), and gdb(WIP) APIs to directly inject point clouds through inter-process communication" >> $control_file
-echo "" >> $control_file
+echo  "" >> $control_file
 dpkg-deb --build $taget_base
 dpkg-deb -c $taget_base.deb
 
 rm -rf $taget_base
 
-
+bazel run --stamp --embed_label=$ver -- //packaging:wheel.publish --repository pypi
