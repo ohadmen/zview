fa0937c7a11b6cc9281b4da40f9a4836e98bca2a
Ohad Menashe <ohad.men@gmail.com>
2024-09-19 21:51:07 +0200

    compilation mode opt

--- packaging/create_debian_package.sh
+++ packaging/create_debian_package.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-bazel build //packaging:wheel ||exit
-bazel build //packaging:debian-package ||exit
+bazel build //packaging:wheel  --compilation_mode=opt ||exit
+bazel build //packaging:debian-package --compilation_mode=opt ||exit
 
 # bazel run --stamp --embed_label=$ZVIEW_VERSION -- //packaging:wheel.publish --repository pypi
