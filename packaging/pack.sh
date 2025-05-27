98d80138184a7697b0d9107257ebe5151c8c0b00
Ohad Menashe <ohad.men@gmail.com>
2025-05-27 21:56:56 +0200

    wip

--- packaging/pack.sh
+++ packaging/pack.sh
@@ -4,11 +4,11 @@
 docker run  --rm -it -e PLAT=manylinux_2_28_x86_64 --entrypoint bash  -v $PWD:/io quay.io/pypa/manylinux_2_28_x86_64  /io/packaging/build_wheels.sh
 twine upload wheelhouse/*
 
-# debian package
-#debuild -S -sa
-debuild -us -uc #local
 
-cd ..
+# debian package
+debuild -S -sa # upload
 # find first file that ends with .changes
-TARGET_FILE=$(ls -t *_source.changes | head -n 1)
+TARGET_FILE=$(ls -t ../*_source.changes | head -n 1)
 dput ppa:ohadmen/zview $TARGET_FILE
+#local - optional
+debuild -us -uc 
