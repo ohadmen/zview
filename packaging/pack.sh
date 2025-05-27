de2e45916711a64c04e030c7932eb4add4732d9d
Ohad Menashe <ohad.men@gmail.com>
2025-05-27 21:28:37 +0200

    wip

--- packaging/pack.sh
+++ packaging/pack.sh
@@ -5,5 +5,10 @@ docker run  --rm -it -e PLAT=manylinux_2_28_x86_64 --entrypoint bash  -v $PWD:/i
 twine upload wheelhouse/*
 
 # debian package
-debuild -S -sa
+#debuild -S -sa
+debuild -us -uc #local
 
+cd ..
+# find first file that ends with .changes
+TARGET_FILE=$(ls -t *_source.changes | head -n 1)
+dput ppa:ohadmen/zview $TARGET_FILE
