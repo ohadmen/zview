ef9c83f29cafb8080ddce1ac55f66b034996ad93
Ohad Menashe <ohad.men@gmail.com>
2025-06-01 09:38:24 +0200

    wip

--- packaging/pack.sh
+++ packaging/pack.sh
@@ -4,11 +4,11 @@
 docker run  --rm -it -e PLAT=manylinux_2_28_x86_64 --entrypoint bash  -v $PWD:/io quay.io/pypa/manylinux_2_28_x86_64  /io/packaging/build_wheels.sh
 twine upload wheelhouse/*
 
+#get bazel binary 
+# wget https://github.com/bazelbuild/bazel/releases/download/8.2.1/bazel-8.2.1-linux-x86_64 .
 
 # debian package
-debuild -S -sa # upload
+debuild -S -sd # upload
 # find first file that ends with .changes
 TARGET_FILE=$(ls -t ../*_source.changes | head -n 1)
 dput ppa:ohadmen/zview $TARGET_FILE
-#local - optional
-debuild -us -uc 
